06beb77299f384362f04cb84a5073976a15e55b3
TheNoeTrevino <noetrevino.music@gmail.com>
2025-10-12 18:16:34 -0500

    add(go): queries in function declarations

---
+++ queries/go/injections.scm
@@ -0,0 +1,47 @@
+;  extends
+
+
+; this is directly after the function declaration
+; // language: sql
+; insertEntryQuery := `
+;  INSERT INTO note_game_entries (
+;    user_id,
+;  )
+;  VALUES (
+;    :user_id,
+;  )
+;  RETURNING
+;    id
+;  `
+(
+  (comment) @injection.language
+  .
+  (statement_list 
+    (short_var_declaration 
+      left: (expression_list 
+        (identifier)) 
+      right: (expression_list 
+        (raw_string_literal 
+          (raw_string_literal_content) @injection.content  )))) 
+  (#set! injection.include-children)
+  (#set! injection.combined)
+  (#gsub! @injection.language "^//%s*language:%s*(%w+).*$" "%1")
+  (#set! priority 201)
+)
+
+; same as above, but can be after the function declaration 
+(
+  (comment) @injection.language
+  .
+  (short_var_declaration 
+    left: (expression_list 
+      (identifier)) 
+    right: (expression_list 
+      (raw_string_literal 
+          (raw_string_literal_content) @injection.content ))) 
+
+  (#set! injection.include-children)
+  (#set! injection.combined)
+  (#gsub! @injection.language "^//%s*language:%s*(%w+).*$" "%1")
+  (#set! priority 201)
+)
